4d7d8f4ab1c1eb0669c3e5d46be1ece787863fae
unknown <goncalves2541@gmail.com>
2021-04-05 01:24:17 +0100

    Home Page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// import { Container } from './styles';
+import SideBar from '../../components/Sidebar';
+
+function Home() {
+  return <SideBar />;
+}
+
+export default Home;
